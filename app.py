0bd6208dff572573885ba955182c2698e3002d99
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2024-12-06 19:14:04 +0100

    Add Redis dependency to flask app

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
-# import redis
+import redis
 
 app = Flask(__name__)
-# cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
-    # retries = 5
-    # while True:
-    #     try:
-    #         return cache.incr('hits')
-    #     except redis.exceptions.ConnectionError as exc:
-    #         if retries == 0:
-    #             raise exc
-    #         retries -= 1
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
     return 1
 
 @app.route('/')
